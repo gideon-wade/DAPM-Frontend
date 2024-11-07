6b4d6d493c79ff5f60e2f2d5f24a6cfbea78ef3e
Zaygone <46322096+Zaygone@users.noreply.github.com>
2024-11-07 15:38:50 +0100

    added renaming to cards

--- src/components/OverviewPage/PipelineCard.tsx
+++ src/components/OverviewPage/PipelineCard.tsx
@@ -3,13 +3,16 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea, IconButton } from '@mui/material';
+import { CardActionArea, IconButton, TextField } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setActivePipeline } from '../../redux/slices/pipelineSlice';
 import { useDrag, useDrop } from 'react-dnd';
 import SourceIcon from '@mui/icons-material/Source';
+import EditIcon from '@mui/icons-material/Edit';
+import { useState, useRef } from "react";
+import { updatePipelineName } from "../../redux/slices/pipelineSlice";
 
 const ItemType = 'CARD';
 
@@ -29,7 +32,8 @@ export interface PipelineCardProps {
 export default function PipelineCard({ id, name, imgData, index, isFolder, folderID, moveCard, moveCardToFolder, goToFolder, onDelete }: PipelineCardProps) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
+  const [isEditing, setIsEditing] = useState(false);
+  const textFieldRef = useRef<HTMLInputElement>(null);
   const navigateToPipeline = () => {
     if(!isFolder) {
       dispatch(setActivePipeline(id));
@@ -42,6 +46,21 @@ export default function PipelineCard({ id, name, imgData, index, isFolder, folde
   const handleDelete = () => {
     onDelete(id); 
   };
+
+  const handleStartEditing = () => {
+    dispatch(setActivePipeline(id));
+    setIsEditing(true);
+  };
+
+  const handleFinishEditing = () => {
+    setIsEditing(false);
+    textFieldRef.current?.blur();
+  };
+
+  const setPipelineName = (name: string) => {
+    dispatch(updatePipelineName(name))
+  }
+
   const [{ isDragging }, drag] = useDrag({
     type: ItemType,
     item: { id, index, isFolder, folderID },
@@ -73,10 +92,10 @@ export default function PipelineCard({ id, name, imgData, index, isFolder, folde
   });
 
   return (
-    <div ref={(node) => drag(drop(node))} style={{ opacity: isDragging ? 0.5 : 1 }}>
+    <div ref={(node) => !isEditing && drag(drop(node))} style={{ opacity: isDragging ? 0.5 : 1 }}>
       <Card sx={{ maxWidth: 345 }}>
         <CardActionArea>
-          <CardMedia
+        <CardMedia
             sx={{ 
               height: 140, 
               display: 'flex', 
@@ -84,25 +103,46 @@ export default function PipelineCard({ id, name, imgData, index, isFolder, folde
               justifyContent: 'center',
               backgroundColor: isFolder ? 'rgba(0, 0, 0, 0.08)' : 'inherit'
             }}
-            title={name}
-            onClick={navigateToPipeline}
+            title={isEditing ? undefined : name}
+            onClick={isEditing ? undefined : navigateToPipeline}
           >
-            {isFolder ? (
-              <SourceIcon sx={{ fontSize: 64, color: '#1976d2' }} />
+            {isEditing ? (
+              <TextField
+                value={name}
+                onChange={(event) => setPipelineName(event?.target.value as string)}
+                autoFocus
+                onBlur={handleFinishEditing}
+                inputProps={{ style: { textAlign: 'center', width: 'auto' } }}
+                onKeyDown={(event) => {
+                  if (event.key === 'Enter') {
+                    handleFinishEditing();
+                  }
+                }}
+                inputRef={textFieldRef} 
+              />
             ) : (
-              <img src={imgData} alt={name} style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }} />
+              isFolder ? (
+                <SourceIcon sx={{ fontSize: 64, color: '#1976d2' }} />
+              ) : (
+                <img src={imgData} alt={name} style={{ maxWidth: '100%', maxHeight: '100%', objectFit: 'contain' }} />
+              )
             )}
           </CardMedia>
           <CardContent>
-            <Typography gutterBottom variant="h5" component="div" onClick={navigateToPipeline}>
+            <Typography 
+              gutterBottom 
+              variant="h5" 
+              component="div" 
+              onClick={!isEditing ? navigateToPipeline : undefined} 
+            >
               {name}
             </Typography>
             <IconButton
-              aria-label="delete"
-              onClick={handleDelete}
+              aria-label="rename"
+              onClick={handleStartEditing}
               sx={{
                 position: 'absolute',
-                top: 8,
+                top: 52,
                 right: 8,
                 color: '#96281b',
                 backgroundColor: 'rgba(0, 0, 0, 0.35)',
@@ -111,14 +151,38 @@ export default function PipelineCard({ id, name, imgData, index, isFolder, folde
                 },
               }}
             >
-              <DeleteIcon />
+              <EditIcon />
             </IconButton>
+            {!isEditing && ( 
+              <IconButton
+                aria-label="delete"
+                onClick={handleDelete}
+                sx={{
+                  position: 'absolute',
+                  top: 8,
+                  right: 8,
+                  color: '#96281b',
+                  backgroundColor: 'rgba(0, 0, 0, 0.35)',
+                  '&:hover': {
+                    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+                  },
+                }}
+              >
+                <DeleteIcon />
+              </IconButton>
+            )}
           </CardContent>
-          <Typography variant="body2" color="text.secondary" onClick={navigateToPipeline} sx={{ padding: '0 16px 16px' }}>
+          <Typography 
+            variant="body2" 
+            color="text.secondary" 
+            onClick={!isEditing ? navigateToPipeline : undefined} 
+            sx={{ padding: '0 16px 16px' }}
+          >
             {isFolder ? 'Click to open folder' : 'Click to modify the pipeline'}
           </Typography>
         </CardActionArea>
       </Card>
     </div>
   );
+  
 }
