434999f998fda3dbb7e38be5e272fb023fd89fd6
AndersKelPou <anders.poulsen.kp@gmail.com>
2024-11-08 12:51:40 +0100

    completed logout

--- src/components/OverviewPage/Buttons/LogoutButton.tsx
+++ src/components/OverviewPage/Buttons/LogoutButton.tsx
@@ -1,13 +1,14 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from '@mui/material';
 import React from "react";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+    <Button variant="contained" style={{float: 'right'}} className="float-right" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
       Log Out
-    </button>
+    </Button>
   );
 };
 
